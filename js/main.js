fb730c8465bc60a8cae8a14d536a8793238d7334
Calvin Ly <lycalvin99@gmail.com>
2019-04-28 00:22:43 -0700

    loading screen

--- js/main.js
+++ js/main.js
@@ -1,13 +1,125 @@
 // LOADING SCREEN
-window.addEventListener("load", function(){
-    var loading = document.getElementById("load_screen");
+$(document).ready(function() {
     setTimeout(function(){
-        document.body.removeChild(loading);
-    }, 1800)
+        $("#loading").fadeToggle(1000, function(){
+            $("#loading").css("display","none");
+            $("#main").css("display","inline");
+            particlesJS('particles-js',
+              {
+                "particles": {
+                  "number": {
+                    "value": 50,
+                    "density": {
+                      "enable": true,
+                      "value_area": 800
+                    }
+                  },
+                  "color": {
+                    "value": "#ffffff"
+                  },
+                  "shape": {
+                    "type": "circle",
+                    "stroke": {
+                      "width": 0,
+                      "color": "#000000"
+                    },
+                    "polygon": {
+                      "nb_sides": 2
+                    },
+                    "image": {
+                      "src": "img/github.svg",
+                      "width": 100,
+                      "height": 100
+                    }
+                  },
+                  "opacity": {
+                    "value": 0.7,
+                    "random": false,
+                    "anim": {
+                      "enable": false,
+                      "speed": 1,
+                      "opacity_min": 0.1,
+                      "sync": false
+                    }
+                  },
+                  "size": {
+                    "value": 3.5,
+                    "random": true,
+                    "anim": {
+                      "enable": true,
+                      "speed": 15,
+                      "size_min": 0.7,
+                      "sync": true
+                    }
+                  },
+                  "line_linked": {
+                    "enable": false,
+                    "distance": 150,
+                    "color": "#ffffff",
+                    "opacity": 0.4,
+                    "width": 1
+                  },
+                  "move": {
+                    "enable": true,
+                    "speed": 4,
+                    "direction": "top-right",
+                    "random": true,
+                    "straight": false,
+                    "out_mode": "out",
+                    "attract": {
+                      "enable": false,
+                      "rotateX": 600,
+                      "rotateY": 1200
+                    }
+                  }
+                },
+                "interactivity": {
+                  "detect_on": "canvas",
+                  "events": {
+                    "onhover": {
+                      "enable": true,
+                      "mode": "repulse"
+                    },
+                    "onclick": {
+                      "enable": true,
+                      "mode": "push"
+                    },
+                    "resize": true
+                  },
+                  "modes": {
+                    "grab": {
+                      "distance": 400,
+                      "line_linked": {
+                        "opacity": 1
+                      }
+                    },
+                    "bubble": {
+                      "distance": 200,
+                      "size": 15,
+                      "duration": 2,
+                      "opacity": 8,
+                      "speed": 3
+                    },
+                    "repulse": {
+                      "distance": 200
+                    },
+                    "push": {
+                      "particles_nb": 50
+                    },
+                    "remove": {
+                      "particles_nb": 2
+                    }
+                  }
+                },
+                "retina_detect": true
+              }
+
+            );
+        });
+    }, 2000);
 });
 
 // JQUERY
-
 $(function(){
     $("#navbar").hide();
     $("#about-hide").hide();
